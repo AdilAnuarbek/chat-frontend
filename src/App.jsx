22e7c7ed1438fbcc106b20062fb2b6920fd7b8d3
Adil Anuarbek <adilanuarbek3@gmail.com>
2025-08-07 19:01:03 +0500

    Fixed time and added .env

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import React, { useEffect, useState } from 'react';
-
 function App() {
 	const [ws, setWs] = useState(null);
 	const [messages, setMessages] = useState([]);
 	const [input, setInput] = useState("");
 	let nickname = localStorage.getItem("nickname")
+	// const messagesEndRef = useRef(null);
+
 	useEffect(() => {
-		// const socket = new WebSocket("wss://chat-backend-67nq.onrender.com/ws");
-		const socket = new WebSocket("ws://localhost:8080/ws");
+		const socket = new WebSocket(import.meta.env.VITE_WEBSOCKET_URL);
 		setWs(socket);
 
 		socket.onmessage = (event) => {
 			const data = JSON.parse(event.data);
 			const senderNickname = data.nickname || "Anonymous";
+			const time = data.timestamp;
 			
 			const message = data.message || "";
-			setMessages((prev) => [...prev, { senderNickname, message }]);
+			setMessages((prev) => [...prev, { senderNickname, message, time }]);
 		};
 
 		return () => socket.close();
@@ -42,7 +43,7 @@ function App() {
 			<h1 className="text-xl font-bold mb-4">Real-time Chat</h1>
 			<div className="border h-64 overflow-y-scroll mb-4 p-2">
 				{messages.map((msg, i) => (
-					<div key={i} className="mb-1">{msg.senderNickname + ": " + msg.message}</div>
+					<div key={i} className="mb-1">{msg.time + " " + msg.senderNickname + ": " + msg.message}</div>
 				))}
 			</div>
 			<input
